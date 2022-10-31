4f128a50a3317ea5f47b0ee1147310ec979f36aa
qwer9052 <qwer9052@gmail.com>
2022-10-31 20:52:10 +0900

    [Add] 프로젝트 첫 커밋

---
+++ src/test/java/com/module/domain/ModuleDomainUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleDomainUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
